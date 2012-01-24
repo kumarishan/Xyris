f38df5d1124e88a83ed42916d19b85a12e462526
Kumar Ishan <kumar.ishan.opensource@gmail.com>
2012-01-24 12:47:46 +0530

    deleted all the file for recreating using eclipse setup

--- xyris-brain/src/main/java/com/xyris/HelloScala.java
+++
@@ -1,9 +0,0 @@
-package com.xyris;
-
-public class HelloScala {
-  public static void helloScala(){
-    System.out.println("hello world from java");
-    System.out.println("Scala says: " + App.helloJava());
-  }
-}
-
